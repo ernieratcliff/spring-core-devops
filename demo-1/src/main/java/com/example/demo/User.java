c361aad5e98f3dd800c728444dc46267a7df14a4
ernie.ratcliff@gmail.com <ernie.ratcliff@gmail.com>
2021-12-26 08:58:51 -0500

    added db connection

---
+++ demo-1/src/main/java/com/example/demo/User.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique=true, name="user_id")
+    private Long id;
+ 
+    @Column(nullable = false)
+    private String name;
+    
+ 
+    @Column(nullable = false)
+    private String email;
+
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
+	
+	
+}
